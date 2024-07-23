44a608bfd643d4aa930109a1fa3fd4052b005b71
Rohin Vig <vohinr@icloud.com>
2024-07-23 12:51:42 -0700

    Added Comments & Scatter Plot of Violations vs. Year

--- RohinProject/Scripts/EDA_dataCleaning.R
+++ RohinProject/Scripts/EDA_dataCleaning.R
@@ -3,26 +3,29 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 library(tidyverse)
-getwd()
 
-# read in dataset####
+#CLEAN COLUMNS
+  #read in dataset####
 df <- read_csv("/Users/rohinv/Downloads/DSRP-2024-Jason/data/DOHMH_New_York_City_Restaurant_Inspection_Results_20240721.csv.zip")
 
-#clean column names of the dataset####
+  #clean column names of the dataset####
 names(df)
 
-#change column case to snakecase####
+#change column case to snakecase
 df_cleanCol <- df |>
  clean_names(case = "snake")
 
 
-#filter data for only 2023####
+
+#PREPARE DATA FOR VISUALIZATION
+  #filter data for only 2023####
+      #seperate the inspection_date column into day, month, and year to filter based on date
 df_2023 <- df_cleanCol|>
   separate(inspection_date, into=c("day", "month", "year_of_inspection"), sep = "/")|>
   filter(year_of_inspection == 2023)
-df_2023
 
-#visualizing restaurant abundance per borough in 2023####
+#VISUALIZE DATA
+  #visualizing restaurant abundance per borough in 2023####
 df_2023|>
   ggplot(aes(boro, fill = grade)) + geom_bar() + 
   labs(x = "Borough", y = "Number of Restaurants", title = "Number of restaurants in each New York Borough in 2023")
@@ -35,7 +38,11 @@ df_2023|>
   labs(x = "Borough", y = "Number of Restaurants", title = "Number of restaurants in each New York Borough in 2023")
   
 
-#Pie Chart for proportion of valid grades in New York in 2023####
+
+#HOW MUCH DOES THE GRADING IN DIFFERENT BOROUGHS DIFFER BY?
+
+#PIE CHARTS COMPARING GRADE PROPORTIONS IN NYC BOROUGHS
+  #Pie Chart for proportion of valid grades in New York in 2023####
 df_ny <- df_2023 |>
   filter(!is.na(grade))
 
@@ -46,7 +53,7 @@ counts <- grade_counts_ny$count
 grade <- grade_counts_ny$grade
 
 pie(counts, main = "Grade of Restaurants in New York City", labels = grade)
-#Manhattan####
+  #Manhattan####
 df_manhattan <- df_2023 |>
   filter(boro == "Manhattan", !is.na(grade))
 
@@ -58,7 +65,7 @@ grade <- grade_counts_manhattan$grade
 
 pie(counts, main = "Grade of Restaurants in Manhattan", labels = grade)
 
-#Brooklyn ####
+  #Brooklyn ####
 df_brooklyn <- df_2023 |>
   filter(boro == "Brooklyn", !is.na(grade))
 
@@ -70,7 +77,7 @@ grade <- grade_counts_brooklyn$grade
 
 pie(counts, main = "Grade of Restaurants in Brooklyn", labels = grade)
 
-#Bronx####
+  #Bronx####
 df_bronx <- df_cleanCol |>
   filter(boro == "Bronx", !is.na(grade))
 
@@ -81,7 +88,7 @@ counts <- grade_counts_bronx$count
 grade <- grade_counts_bronx$grade
 
 pie(counts, main = "Grade of Restaurants in The Bronx", labels = grade)
-#Queens####
+  #Queens####
 df_Q <- df_cleanCol |>
   filter(boro == "Queens", !is.na(grade))
 
@@ -93,7 +100,7 @@ grade <- grade_counts_Q$grade
 
 pie(counts, main = "Grade of Restaurants in Queens", labels = grade)
 
-#Staten Island####
+  #Staten Island####
 df_S <- df_cleanCol |>
   filter(boro == "Staten Island", !is.na(grade))
 
@@ -106,7 +113,9 @@ grade <- grade_counts_S$grade
 pie(counts, main = "Grade of Restaurants in Staten Island", labels = grade)
 
 
-#Checking whether a restaurant is graded and has a value that is N/A and vice versa####
+
+#CHECKING FOR ANOMALOUS ENTRIES
+  #Checking whether a restaurant is graded and has a value that is N/A and vice versa####
 df_graded_irregularly <- df_cleanCol|>
   filter(!is.na(grade), inspection_date == "01/01/1900")
 
@@ -117,13 +126,19 @@ df_graded_irregularly2
 df_graded_irregularly
 
 
-#total number of violation counts per in New York vs. year####
 
+#HOW HAVE THE NUMBER OF VIOLATIONS PER RESTAURANT CHANGED OVER THE LAST FEW YEARS?
+  #average number of violation counts per in New York vs. year####
+  #filter NA placeholders from both variables and arrange dataframe by year of inspection
 df_byYear <- df_cleanCol|>
   separate(inspection_date, into=c("day", "month", "year_of_inspection"), sep = "/")|>
-  filter(year_of_inspection != 1900)|>
+  filter(year_of_inspection != 1900, !is.na(score), score != "NA")|>
   arrange(year_of_inspection)
 
-df_byYear|>
-  summarise(sum(score))
+  #create a table with columns year_of_inspection and mean number of violations per restaurant
+df_year_vs_violations <- summarise(group_by(df_byYear, year_of_inspection), sum(score)/n())
   
+  #create scatter plot
+df_year_vs_violations|>
+  ggplot(aes(year_of_inspection, `mean(score)`)) + geom_point()
+
