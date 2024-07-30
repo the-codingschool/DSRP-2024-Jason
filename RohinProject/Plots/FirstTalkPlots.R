8e2aa08630133bca57754609cb17dbd23b4009a0
Rohin Vig <vohinr@icloud.com>
2024-07-30 10:11:32 -0700

    Made the visualizations clearer

--- RohinProject/Plots/FirstTalkPlots.R
+++ RohinProject/Plots/FirstTalkPlots.R
@@ -22,22 +22,41 @@ df_zipcodeScoreVisualization |>
 #04M: roaches
 #04N: Flies
 
-Code = "04K"
+Code <- "04N"
 
-#Summarize the data based on zip code and number of 04_ violations
-df_vermin <- df_clean|>
-  filter(violation_code == Code)|>
-  group_by(zipcode)|>
-  summarise(n = n())
+#create two dataframes with number of restaurants and number of infested restaurants by zipcode
+df_total_restaurants <- df_clean |>
+  group_by(zipcode) |>
+  summarise(total_restaurants = n())
 
-#Join the Number of 04_ violations to the original data
-df_VerminVisualization <- left_join(df_clean, df_vermin, by = "zipcode")
+df_vermin <- df_clean |>
+  filter(violation_code == Code) |>
+  group_by(zipcode) |>
+  summarise(vermin_restaurants = n())
+
+#use the total number of restaurants in the zipcode and the vermin infested restarurants and find the percent of restaurants in the zipcode infested
+df_merged <- left_join(df_vermin, df_total_restaurants, by = "zipcode")
+df_merged <- df_merged |>
+  mutate(percent_vermin = (vermin_restaurants / total_restaurants) * 100)
+df_merged <- left_join(df_clean, df_merged, by = "zipcode")
 
 #Plot the data
-df_VerminVisualization|>
-  filter(boro == "manhattan")|>
-  ggplot(aes(x = longitude, y = latitude, color = n)) + geom_point() + labs(title = "Number of Restaurants in Manhattan with a Rat Violation by Zipcode", x = "Longitude", y = "Latitude")
-
+df_merged |>
+  filter(boro == "manhattan", vermin_restaurants != 0) |>
+  ggplot(aes(x = longitude, y = latitude, color = percent_vermin)) +
+  geom_point() +
+  scale_color_gradient(low = "blue", high = "orange") +
+  labs(title = "  Percent of Restaurants in Manhattan with Rat Infestation by Zipcode", x = "Longitude", y = "Latitude", color = "Percentage of Restaurants") +
+  theme_classic() + 
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.ticks.y = element_blank(),
+    panel.grid = element_blank()
+  )
 
 #Perceent of restaurants in different cuisines with a vermin violation in 2023####
   #04K: rats
@@ -45,7 +64,7 @@ df_VerminVisualization|>
   #04M: roaches
   #04N: Flies
 
-Code1 <- "04K"
+Code1 <- "05A"
 
 #Compute the count by cuisine
 df_count_by_cuisine <- df_clean |>
@@ -72,5 +91,5 @@ df_VerminByCuisine |>
   filter()|>
   ggplot(aes(x = cuisine_description, y = n, fill = cuisine_description)) + 
   geom_bar(stat = "identity") + 
-  labs(x = "Cuisine", y = "Instances of Rat Violations in 2023", title = "Percent of Restaurants with Rat Violations in 2023 by Cuisine") +
+  labs(x = "Cuisine", y = "Instances of Rat Violations in 2023", title = "Percent of Restaurants with Roach Violations in 2023 by Cuisine", color = "Cuisine") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
