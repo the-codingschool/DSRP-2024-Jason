76844a97a4e8f0cc1d6366692d13a8b53d373378
Rohin Vig <vohinr@icloud.com>
2024-07-31 12:04:59 -0700

    fixed bug relating to violation code summary

--- RohinProject/Scripts/Analysis1.R
+++ RohinProject/Scripts/Analysis1.R
@@ -5,6 +5,7 @@ library(downloader)
 library(caTools)
 library(FNN)
 library(readxl)
+library(dplyr)
 
 #KNN####
 df_pestViolations <- df_clean|>
@@ -33,6 +34,8 @@ cat('\nAccuracy:', accuracy, '\n')
 
 
 
+
+
 #one hot encode cuisine description####
 df_onehot <- df_pestViolations|>
   filter(cuisine_description %in% c("chinese/cuban", "african", "new american", "vegetarian", "tapas", "armenian", "egyptian", "english", "irish", "turkish", "barbecue", "eastern european", "continental", "indian", "creole/cajun", "salads", "australian", "soul food"))|>
@@ -47,6 +50,8 @@ df_encoded <- df_encoded1|>
   mutate_all(~ ifelse(is.na(.), 0, .))
 
 df_encoded
+
+
 
 #KNN with cuisine####
 #split the data
@@ -67,6 +72,8 @@ accuracy <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
 cat('Confusion Matrix:\n')
 print(confusion_matrix)
 cat('\nAccuracy:', accuracy, '\n')
+
+
 
 
 
@@ -114,6 +121,8 @@ chisq.test(chisq_mice)
 chisq.test(chisq_roaches)
 chisq.test(chisq_flies)
 
+
+
 ##Chi squared test for bad upkeep vs. every pest infestation####
 df_codeencoded <- df_clean|>
   filter(violation_code %in% c("04K", "04L", "04M", "04N", "10F"), boro == "manhattan")|>
@@ -124,26 +133,25 @@ df_codeencoded <- df_clean|>
   filter(longitude != 0, latitude != 0)|>
   arrange(dba)
 
-df_codeencoded
-
 df_joined <- left_join(df_clean, df_codeencoded, by = "dba")|>
   filter(boro == "manhattan")|>
   mutate(`04K` = ifelse(is.na(`04K`), 0, `04K`))|>
-  mutate(`10F` = ifelse(is.na(`10F`), 0, `10F`))|>
   mutate(`04L` = ifelse(is.na(`04L`), 0, `04L`))|>
   mutate(`04M` = ifelse(is.na(`04M`), 0, `04M`))|>
-  mutate(`04N` = ifelse(is.na(`04N`), 0, `04N`))
+  mutate(`04N` = ifelse(is.na(`04N`), 0, `04N`))|>
+  mutate(`10F` = ifelse(is.na(`10F`), 0, `10F`))
 
-df_joined$`10F`
+
+cols <- c("10F", "04K", "04L", "04M", "04N")
 
 df_codeencoded <- df_joined|>
   group_by(dba) |>
-  summarise(across(starts_with("0"), ~sum(., na.rm = TRUE)))|>
+  summarise(across(all_of(cols), ~sum(., na.rm = TRUE)))|>
   mutate(`04K` = ifelse(`04K` != 0 & `04K` != 1, 1, `04K`))|>
-  mutate(`10F` = ifelse(`10F` != 0 & `10F` != 1, 1, `10F`))|>
   mutate(`04L` = ifelse(`04L` != 0 & `04L` != 1, 1, `04L`))|>
   mutate(`04M` = ifelse(`04M` != 0 & `04M` != 1, 1, `04M`))|>
-  mutate(`04N` = ifelse(`04N` != 0 & `04N` != 1, 1, `04N`))
+  mutate(`04N` = ifelse(`04N` != 0 & `04N` != 1, 1, `04N`))|>
+  mutate(`10F` = ifelse(`10F` != 0 & `10F` != 1, 1, `10F`))
 
 df_codeencoded
 
@@ -156,5 +164,4 @@ chisq.test(chisq_rats)
 chisq.test(chisq_mice)
 chisq.test(chisq_roaches)
 chisq.test(chisq_flies)
-
 
