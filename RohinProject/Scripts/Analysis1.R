d09a978027b0c97dfe0f224683d32542c942c5a5
Rohin Vig <vohinr@icloud.com>
2024-07-31 10:43:02 -0700

    added Chi squared test for Conducive to Pests vs. every pest infestation

--- RohinProject/Scripts/Analysis1.R
+++ RohinProject/Scripts/Analysis1.R
@@ -1,3 +1,11 @@
+library(ggplot2)
+library(caTools)
+library(readr)
+library(downloader)
+library(caTools)
+library(FNN)
+library(readxl)
+
 #KNN####
 df_pestViolations <- df_clean|>
   filter(violation_code %in% c("04K", "04L", "04M", "04N"), !is.na(latitude), !is.na(longitude), boro == "manhattan")
@@ -22,6 +30,7 @@ accuracy <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
 cat('Confusion Matrix:\n')
 print(confusion_matrix)
 cat('\nAccuracy:', accuracy, '\n')
+
 
 
 #one hot encode cuisine description####
@@ -58,3 +67,44 @@ accuracy <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
 cat('Confusion Matrix:\n')
 print(confusion_matrix)
 cat('\nAccuracy:', accuracy, '\n')
+
+
+
+#Chi squared test for Conducive to Pests vs. every pest infestation####
+df_codeencoded <- df_clean|>
+  filter(violation_code %in% c("04K", "04L", "04M", "04N", "08A"), boro == "manhattan")|>
+  mutate(value = 1) |>
+  spread(key = violation_code, value = value, fill = 0)|>
+  select(c("dba", "zipcode", "latitude", "longitude", "04K", "04L", "04M", "04N", "08A"))|>
+  distinct()|>
+  filter(longitude != 0, latitude != 0)|>
+  arrange(dba)
+
+df_joined <- left_join(df_clean, df_codeencoded, by = "dba")|>
+  filter(boro == "manhattan")|>
+  mutate(`04K` = ifelse(is.na(`04K`), 0, `04K`))|>
+  mutate(`08A` = ifelse(is.na(`08A`), 0, `08A`))|>
+  mutate(`04L` = ifelse(is.na(`04L`), 0, `04L`))|>
+  mutate(`04M` = ifelse(is.na(`04M`), 0, `04M`))|>
+  mutate(`04N` = ifelse(is.na(`04N`), 0, `04N`))
+
+df_codeencoded <- df_joined|>
+  group_by(dba) |>
+  summarise(across(starts_with("0"), ~sum(., na.rm = TRUE)))|>
+  mutate(`04K` = ifelse(`04K` != 0 & `04K` != 1, 1, `04K`))|>
+  mutate(`08A` = ifelse(`08A` != 0 & `08A` != 1, 1, `08A`))|>
+  mutate(`04L` = ifelse(`04L` != 0 & `04L` != 1, 1, `04L`))|>
+  mutate(`04M` = ifelse(`04M` != 0 & `04M` != 1, 1, `04M`))|>
+  mutate(`04N` = ifelse(`04N` != 0 & `04N` != 1, 1, `04N`))
+
+chisq_rats <- table(df_codeencoded$`08A`, df_codeencoded$`04K`) 
+chisq_mice <- table(df_codeencoded$`08A`, df_codeencoded$`04L`) 
+chisq_roaches <- table(df_codeencoded$`08A`, df_codeencoded$`04M`) 
+chisq_flies <- table(df_codeencoded$`08A`, df_codeencoded$`04N`) 
+
+chisq.test(chisq_rats)
+chisq.test(chisq_mice)
+chisq.test(chisq_roaches)
+chisq.test(chisq_flies)
+
+##Chi squared test for bad upkeep vs. every pest infestation
